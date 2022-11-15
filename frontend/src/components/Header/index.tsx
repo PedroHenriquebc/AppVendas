7f25cb02dea0c3b6c0ddce3984137ff504958bb1
Pedro Henrique <pedrohbc96@gmail.com>
2022-11-15 17:26:55 -0300

    Adicionando outros componentes

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="DSMeta" />
+                <h1>VendasApp</h1>
+            </div>
+        </header>
+    )
+}
+
+export default Header
